4bbd3a63eab65f22928b3c0d1672725c949a8021
phanhuudang <dangph@apzon.com>
2022-09-06 08:40:59 +0700

    add message code

--- QTS.API/QTS.Commons/HttpResult.cs
+++ QTS.API/QTS.Commons/HttpResult.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,8 +36,50 @@ namespace QTS.Commons
     }
     public enum MessageCode
     {
-        Success=200,
-        NotFound=404,
-        Error
+        [Description("None")]
+        None = 0,
+
+        [Description("Success")]
+        Success = 200,
+
+        [Description("Error")]
+        Error = 2,
+
+        [Description("Exception")]
+        Exception = 3,
+
+        [Description("Token has been expired")]
+        TokenExpired = 9,
+
+        [Description("Invalid Token")]
+        TokenInvalid = 10,
+
+        [Description("Function not supported")]
+        FunctionNotSupport = 13,
+
+        #region Messgage database
+
+        [Description("Unable access to database")]
+        UnableAccessDatabase = 20,
+
+        [Description("MessageCode in database not consistence with defined enums")]
+        EnumDataInConsistence = 21,
+
+        [Description("object_type not exists in system")]
+        SystemObjectTypeNotExists = 22,
+
+        [Description("MessageCode Enums Duplicated. Please contact administrator")]
+        DuplicateMessageCode = 23,
+
+        [Description("Special characters do not allow.")]
+        SpecialCharacterNotAllow = 24,
+        
+        [Description("Type not supported")]
+        TypeNotSupported = 26,
+
+        [Description("Object type not supported")]
+        ObjectTypeNotSupported = 27,
+        #endregion
+
     }
 }
